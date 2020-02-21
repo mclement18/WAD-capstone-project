607a0588f2c0f676948c304914b281c9d456106c
Mathieu Clément <mcfly1893@gmail.com>
2020-02-21 15:19:02 +0100

    Implement users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        session[:user_id] = @user.id
+        format.html { redirect_to root_path, notice: 'Successfully signed up!' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :name, :avatar)
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get new" do
+    get new_user_url
+    assert :success
+  end
+
+  test "get new via signup route" do
+    get signup_url
+    assert :success
+  end
+
+  test "create new user" do
+    assert_difference('User.count') do
+      post signup_url, params: { user: { email: 'new@wheretogo.com', password: 'password' } }
+    end
+
+    assert_redirected_to root_url
+  end
 end
