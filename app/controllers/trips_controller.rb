0c46e3d35b26dd51e2d1ab2b08b91b8a23367a6a
Mathieu Clément <mcfly1893@gmail.com>
2020-02-25 19:49:49 +0100

    Implement trips#create and basic trips#show view

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -8,7 +8,29 @@ class TripsController < ApplicationController
     end
   end
 
+  def show
+    @trip = Trip.find(params[:id])
+  end
+
   def new
     @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new trip_params
+    @trip.user = User.first
+    respond_to do |format|
+      if @trip.save
+        format.html { redirect_to @trip, notice: 'Trip successfully created' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:title, :description, :category_1, :category_2, :continent, :country, :region, :city, :image)
   end
 end
