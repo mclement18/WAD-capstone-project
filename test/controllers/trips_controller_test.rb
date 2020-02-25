32b7daed379ba2af08defa997c6b6711fd9e2c25
Mathieu Clément <mcfly1893@gmail.com>
2020-02-25 09:22:07 +0100

    Create controller tests for Trips controller

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -1,7 +1,56 @@
 require 'test_helper'
 
 class TripsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @trip = trips(:one)
+  end
+  
+  test "get index" do
+    get trips_url
+    assert :success
+  end
+
+  test "get index with a simple search" do
+    get trips_url, params: { q: 'Trip one' }
+    assert :success
+    assert_select 'h1', 'Trip one'
+  end
+
+  test "get new" do
+    get new_trip_url
+    assert :success
+  end
+
+  test "create new trip" do
+    assert_difference('Trip.count') do
+      post trips_url, params: { trip: { title: 'New trip',
+                                        description: "Trip's description",
+                                        category_1: 'city',
+                                        category_2: 'cultural',
+                                        continent: 'Europe',
+                                        country: 'CH',
+                                        region: 'VD',
+                                        city: 'Lausanne' } }
+    end
+    assert_redirected_to trip_url(Trip.last)
+  end
+
+  test "show trip" do
+    get trip_url(@trip)
+    assert :success
+    assert_select 'h1', @trip.title
+  end
+
+  test "get edit" do
+    get edit_trip_url(@trip)
+    assert :success
+  end
+
+  test "update a trip" do
+    patch trip_url(@trip), params: { trip: { title: 'New title',
+                                             description: 'New description' } }
+    assert_redirected_to trip_url(@trip)
+    assert_select 'h1', 'New title'
+    assert_select 'p.description', 'New description'
+  end
 end
