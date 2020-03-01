13b4e82ce016565906968c6b7ba26c64f34c1cfd
Mathieu Clément <mcfly1893@gmail.com>
2020-03-01 21:21:29 +0100

    Implement stages#index

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -1,2 +1,14 @@
 class StagesController < ApplicationController
+  before_action :ensure_authenticated
+  before_action :load_trip
+  
+  def index
+    @stages = @trip.stages
+  end
+
+  private
+
+  def load_trip
+    @trip = Trip.find(params[:trip_id])
+  end
 end
--- app/helpers/stages_helper.rb
+++ app/helpers/stages_helper.rb
@@ -1,2 +1,9 @@
 module StagesHelper
+  def travel_type_options(selected)
+    options_for_select [['None', 'None'], ['Driving', 'driving'], ['Walking', 'walking'], ['Bicycling', 'bicycling'], ['Transit', 'transit']], selected
+  end
+
+  def is_first_stage?(stage)
+    stage.number == 1
+  end
 end
