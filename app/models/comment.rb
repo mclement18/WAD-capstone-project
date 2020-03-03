312f2fc1db1271b9c0c7763d07d7c4cff6d28066
Mathieu Clément <mcfly1893@gmail.com>
2020-03-03 15:46:14 +0100

    Implement validations for Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :article, polymorphic: true
+
+  validates :body, presence: true, length: { maximum: 300 }
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -2,46 +2,46 @@ require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
   setup do
-    user = users(:one)
-    trip = trips(:one)
+    @user = users(:one)
+    @trip = trips(:one)
   end
   
   test "create a comment for a trip" do
     comment = Comment.new body: 'A new comment',
-                          user: user,
-                          article: trip
+                          user: @user,
+                          article: @trip
     assert comment.save
   end
 
   test "create a comment for a stage" do
     comment = Comment.new body: 'A new comment',
-                          user: user,
+                          user: @user,
                           article: stages(:one)
     assert comment.save
   end
 
   test "body is required" do
-    comment = Comment.new user: user,
-                          article: trip
+    comment = Comment.new user: @user,
+                          article: @trip
     refute comment.valid?
   end
 
   test "body cannot be more than 300 characters" do
     comment = Comment.new body: 'A new comment A new comment A new comment A new comment A new comment A new comment A new comment A new comment A new comment A new comment A new comment A new comment A new comment A new comment A new comment A new comment A new comment A new comment A new comment A new comment A new comment A new comment A new comment A new comment A new comment',
-                          user: user,
-                          article: trip
+                          user: @user,
+                          article: @trip
     refute comment.valid?
   end
 
   test "user is required" do
     comment = Comment.new body: 'A new comment',
-                          article: trip
+                          article: @trip
     refute comment.valid?
   end
 
   test "article is required" do
     comment = Comment.new body: 'A new comment',
-                          user: user
+                          user: @user
     refute comment.valid?
   end
 end
