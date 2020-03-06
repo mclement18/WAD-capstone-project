f28b3fb9d2f54be2a7641134ccfa50d07e6ee2c9
Mathieu Clément <mcfly1893@gmail.com>
2020-03-06 11:46:47 +0100

    Create ToDosController tests

--- test/controllers/to_dos_controller_test.rb
+++ test/controllers/to_dos_controller_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class ToDosControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    sign_in_as('one@wheretogo.com')
+    @user = users(:one)
+  end
+  
+  test "create a todo association" do
+    assert_difference('ToDo.count') do
+      post user_to_dos_url(@user), params: { trip_id: trips(:one).id }, xhr: true
+    end
+    assert_response :success
+  end
+
+  test "update a todo association" do
+    @todo = to_dos(:one)
+    
+    assert_changes('@todo.status', from: 'to-do', to: 'in-progress') do
+      patch user_to_do_url(@user, @todo), params: { transition: 'traveling' }, xhr: true
+    end
+    assert_response
+  end
+
+  test "delete a todo association" do
+    assert_difference('ToDo.count', -1) do
+      delete user_to_do_url(@user, to_dos(:one)), xhr: true
+    end
+    assert_response :success
+  end
 end
