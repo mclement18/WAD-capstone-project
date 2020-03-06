080acde7c15c37ce83f14e51c004bb1f1f9f4368
Mathieu Clément <mcfly1893@gmail.com>
2020-03-06 12:02:32 +0100

    Implement to_dos#create

--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -1,9 +1,17 @@
 class ToDosController < ApplicationController
   before_action :ensure_authenticated
-  before_action :set_todo
+  before_action :set_todo, only: [:update, :destroy]
   
   def create
-    
+    @todo = ToDo.new user: current_user, trip_id: params[:trip_id]
+    respond_to do |format|
+      if @todo.save
+        flash.now.notice = 'Trip successfully added to your To Do List!'
+      else
+        flash.now.alert = 'Unable to add Trip to your To Do List.'
+      end
+      format.js
+    end
   end
 
   def update
