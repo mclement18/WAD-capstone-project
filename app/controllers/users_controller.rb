7914d81da41e0bc9d69bc252ea5d64f5667e146e
Mathieu Clément <mcfly1893@gmail.com>
2020-03-06 17:04:56 +0100

    Implement admin actions for UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :ensure_authenticated, only: [:index, :edit, :update, :destroy]
+  before_action :ensure_admin, only: [:index, :edit, :update, :destroy]
+  before_action :set_user, only: [:edit, :update, :destroy]
+
+  def index
+    @users = User.all.active
+  end
+  
   def new
     @user = User.new
   end
@@ -16,9 +24,49 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update admin_params
+        format.html { redirect_to edit_user_path(@user), notice: 'User succesfully updated!' }
+      else
+        format.html {render :edit }
+      end
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      if @user.soft_delete
+        if @user.id == current_user.id
+          session.delete(:user_id) 
+          format.html { redirect_to root_path, notice: 'You successfuly deleted your account!' }
+        else
+          format.html { redirect_to users_path, notice: 'User successfuly deleted!' }
+        end
+      else
+        format.html { redirect_to users_path, alert: 'Unable to delete user.' }
+      end
+    end
+  end
+  
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def ensure_admin
+    redirect_to root_path unless current_user.role == 'admin'
+  end
 
   def user_params
     params.require(:user).permit(:email, :password, :name, :avatar)
+  end
+
+  def admin_params
+    params.require(:user).permit(:name, :avatar, :role)
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -19,4 +19,36 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
     assert session[:user_id].present?
   end
+
+  test "admin get users list" do
+    sign_in_as('two@wheretogo.com')
+
+    get users_url
+    assert_response :success
+  end
+
+  test "admin get edit" do
+    sign_in_as('two@wheretogo.com')
+
+    get edit_user_url(users(:one))
+    assert_response :success
+  end
+
+  test "admin update user" do
+    sign_in_as('two@wheretogo.com')
+    
+    assert_changes('User.find(users(:one).id).name', from: 'One', to: 'New') do
+      patch user_url(users(:one)), params: { user: { name: 'New' } }
+    end
+    assert_redirected_to edit_user_url(users(:one))
+  end
+
+  test "admin delete user" do
+    sign_in_as('two@wheretogo.com')
+    
+    assert_changes('User.find(users(:one).id).status', from: 'active', to: 'deleted') do
+      delete user_url(users(:one))
+    end
+    assert_redirected_to users_url
+  end
 end
