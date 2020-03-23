f4669be759f50d59dbcf05a276bee2e9c8eb40c9
Mathieu Clément <mcfly1893@gmail.com>
2020-03-23 02:14:15 +0100

    Style the homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,9 @@ module ApplicationHelper
     end
     @alert_id = SecureRandom.hex
   end
+
+  def image_set_tag(source, srcset = {}, options = {})
+    srcset = srcset.map { |src, size| "#{path_to_image(src)} #{size}" }.join(', ')
+    image_tag(source, options.merge(srcset: srcset))
+  end
 end
