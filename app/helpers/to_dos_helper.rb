6f820bc39aa05a5092536bc8390a9aa18abe8ff1
Mathieu Clément <mcfly1893@gmail.com>
2020-03-23 19:03:07 +0100

    Create ToDosHelper methods to load once current usr to_dos

--- app/helpers/to_dos_helper.rb
+++ app/helpers/to_dos_helper.rb
@@ -1,2 +1,21 @@
 module ToDosHelper
+  def current_user_to_dos
+    if @current_user_to_dos
+      return @current_user_to_dos
+    end
+    @current_user_to_dos = current_user.to_dos
+  end
+
+  def current_user_to_do(trip_id)
+    current_user_to_dos.each do |todo|
+      return todo if todo.trip_id == trip_id
+    end
+  end
+  
+  def is_a_current_user_todo?(trip)
+    current_user_to_dos.each do |todo|
+      return true if todo.trip_id == trip.id
+    end
+    false
+  end
 end
