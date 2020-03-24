f49b42e6b7f42a8dce4ac540522a589e82d4b5f2
Mathieu Clément <mcfly1893@gmail.com>
2020-03-24 10:02:14 +0100

    Style categories#index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,9 @@
 class CategoriesController < ApplicationController
   def index
     @category = params[:category]
-    return @trips = Trip.active.load_users.categories_contains([@category]) unless params[:q].present?
+    return @trips = Trip.active.load_users.categories_contains([@category]).page(params[:page]).per(9) unless params[:q].present?
     
     @queries = params[:q].strip.split(' ')
-    @trips = Trip.active.load_users.categories_contains([@category]).global_search(@queries)
+    @trips = Trip.active.load_users.categories_contains([@category]).global_search(@queries).page(params[:page]).per(9)
   end
 end
