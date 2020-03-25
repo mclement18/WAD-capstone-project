723e681d4b676adceaf5c0621070503ecc6efd2c
Mathieu Clément <mcfly1893@gmail.com>
2020-03-25 17:10:02 +0100

    Style input-file button when has error

--- app/assets/javascripts/input.js
+++ app/assets/javascripts/input.js
@@ -1,10 +1,18 @@
 const Input = {};
 
+Input.changeFileTriggerText = function(fileTrigger, inputFile) {
+  fileTrigger.textContent = inputFile.value.split('\\').pop();
+};
+
 Input.addChangeEventToInputFile = function() {
   document.addEventListener('change', event => {
     const target = event.target;
     if (target.classList.contains('input-file')) {
-      target.nextElementSibling.textContent = target.value.split('\\').pop();
+      if (target.nextElementSibling && target.nextElementSibling.id === 'fileTrigger') {
+        this.changeFileTriggerText(target.nextElementSibling, target);
+      } else if (target.parentElement.nextElementSibling.id === 'fileTrigger') {
+        this.changeFileTriggerText(target.parentElement.nextElementSibling, target);
+      }
     }
   });
 };
