e7fffc0ab8c818c86b38a57e244a1f753a2ce3bd
Mathieu Clément <mcfly1893@gmail.com>
2020-03-27 14:00:53 +0100

    Style account#show

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -2,4 +2,8 @@ module AccountHelper
   def is_current?(current, link)
     'current' if current == link
   end
+
+  def account_not_updated?
+    current_user.created_at == current_user.updated_at
+  end
 end
