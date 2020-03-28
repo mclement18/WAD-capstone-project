0b6bea18c84f2aae99864d59862e32665210bc9c
Mathieu Clément <mcfly1893@gmail.com>
2020-03-28 19:09:02 +0100

    Inactive current link in account sub_nav

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -1,7 +1,15 @@
 module AccountHelper
-  def is_current?(current, link)
+  def current_class(current, link)
     'current' if current == link
   end
+  
+    def sub_nav_link_path(path, current, link)
+      if current == link
+        return 'javascript:;'
+      else
+        return path
+      end
+    end
 
   def account_not_updated?
     current_user.created_at == current_user.updated_at
